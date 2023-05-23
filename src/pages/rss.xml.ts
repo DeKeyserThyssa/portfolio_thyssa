20c97a18da5c27f9a578542c71ce02f70befa5a9
DeKeyserThyssa <thyssa.de.keyser@student.howest.be>
2023-05-23 20:45:26 +0200

    add content collections to tutorial

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,18 @@
 import rss, {pagesGlobToRssItems} from "@astrojs/rss";
+import {getCollection} from "astro:content";
 
 export async function get() {
+  const posts = await getCollection("posts");
   return rss({
     title: "Thyssa De Keyser",
     description: "My journey learning Astro",
     site: "https://thyssadekeyser.netlify.app/",
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts.map(post => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `https://thyssadekeyser.netlify.app/posts/${post.slug}`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
